681966c476b0311240d1ea48aec5f7f2256ffdf4
Kenji Fujiwara <niyaton@gmail.com>
2014-09-01 16:06:39 +0900

    now kenja java parser handles array dimension of method parameter

--- src/main/java/jp/naist/sd/kenja/factextractor/ast/ASTMethod.java
+++ src/main/java/jp/naist/sd/kenja/factextractor/ast/ASTMethod.java
@@ -39,6 +39,9 @@ public class ASTMethod implements Treeable{
 		for(Object item: node.parameters()){
 			SingleVariableDeclaration parameter = (SingleVariableDeclaration)item;
 			result.append(parameter.getType().toString());
+			for(int i = 0; i < parameter.getExtraDimensions(); i++){
+				result.append("[]");
+			}
 			//result.append(" ");
 			//result.append(parameter.getName());
 			result.append(",");
