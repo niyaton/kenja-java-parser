5786d1249c959eb77867d6fd0cc4350d8f7c282c
Kenji Fujiwara <kenji-f@is.naist.jp>
2015-01-12 01:38:53 +0900

    added javadoc comment to ASTInterface

--- src/main/java/jp/naist/sd/kenja/factextractor/ast/ASTInterface.java
+++ src/main/java/jp/naist/sd/kenja/factextractor/ast/ASTInterface.java
@@ -1,4 +1,10 @@
 package jp.naist.sd.kenja.factextractor.ast;
 
+/**
+ * A class which represents Field of Java for Historage. This class is not implemented yet.
+ * 
+ * @author Kenji Fujiwara
+ *
+ */
 public class ASTInterface extends ASTType {
 }
