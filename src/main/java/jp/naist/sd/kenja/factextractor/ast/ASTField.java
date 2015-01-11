9906cc6a4408f90d96547b1de539e68e2ae10fba
Kenji Fujiwara <kenji-f@is.naist.jp>
2015-01-12 01:36:48 +0900

    added javadoc comment to ASTField

--- src/main/java/jp/naist/sd/kenja/factextractor/ast/ASTField.java
+++ src/main/java/jp/naist/sd/kenja/factextractor/ast/ASTField.java
@@ -10,14 +10,32 @@ import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 import java.lang.reflect.Modifier;
 
+/**
+ * A class which represents Field of Java for Historage.
+ * 
+ * @author Kenji Fujiwara
+ *
+ */
 public class ASTField implements Blobable {
 
+  /**
+   * map of field name and blobs.
+   */
   private Multimap<String, Blob> fieldMap;
 
+  /**
+   * Default constructor of ASTField.
+   */
   public ASTField() {
     fieldMap = HashMultimap.create();
   }
 
+  /**
+   * Factory Method for ASTField.
+   * 
+   * @param node
+   *          FiledDeclaration node of Eclipse AST
+   */
   public void parseFieldDeclaration(FieldDeclaration node) {
     for (Object obj : node.fragments()) {
       VariableDeclarationFragment fragment = (VariableDeclarationFragment) obj;
@@ -38,11 +56,28 @@ public class ASTField implements Blobable {
     }
   }
 
+  /**
+   * Method for conflicting of field name.
+   * 
+   * @param blob
+   *          Blob of the new conflicted field
+   * @param fieldName
+   *          name of conflicted field
+   * @param numConflicted
+   *          number of conflicted declarations
+   */
   private void conflict(Blob blob, String fieldName, int numConflicted) {
     String newName = fieldName + ".conflicted" + numConflicted;
     blob.setName(newName);
   }
 
+  /**
+   * Return visibility of a field.
+   * 
+   * @param modifier
+   *          modifier number which is given from FieldDeclaration
+   * @return string of field visibility
+   */
   private String getVisibility(int modifier) {
     if (Modifier.isPrivate(modifier)) {
       return "[private]";
