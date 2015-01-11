c6a590072d63e57332dc2959908800e1ee93a753
Kenji Fujiwara <kenji-f@is.naist.jp>
2015-01-12 01:03:58 +0900

    added javadoc comment to ASTCompilation

--- src/main/java/jp/naist/sd/kenja/factextractor/ast/ASTCompilation.java
+++ src/main/java/jp/naist/sd/kenja/factextractor/ast/ASTCompilation.java
@@ -11,30 +11,75 @@ import jp.naist.sd.kenja.factextractor.Treeable;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 
+/**
+ * A class which represents Compilation of Java for Historage.
+ * 
+ * @author Kenji Fujiwara
+ *
+ */
 public class ASTCompilation implements Treeable {
 
+  /**
+   * root Tree of Java file.
+   */
   private Tree root;
 
+  /**
+   * package information of a Java file.
+   */
   private ASTPackage pack;
 
+  /**
+   * Name of root directory which store classes.
+   */
   private final String CLASS_ROOT_NAME = "[CN]";
 
+  /**
+   * Name of root directory which store interfaces.
+   */
   private final String INTERFACE_ROOT_NAME = "[IN]";
 
+  /**
+   * root Tree of classes.
+   */
   private Tree classRoot;
 
+  /**
+   * List of ASTClass.
+   */
   private List<ASTClass> classes = new LinkedList<ASTClass>();
 
+  /**
+   * root Tree of interfaces.
+   */
   private Tree interfaceRoot;
 
+  /**
+   * Default constructor for ASTCompilation.
+   */
   protected ASTCompilation() {
 
   }
 
+  /**
+   * Factory method for creating a ASTCompilation instance.
+   * 
+   * @param unit
+   *          CompilationUnit of Eclipse AST.
+   * @return ASTCompilation instance which is corresponding to unit.
+   */
   public static ASTCompilation fromCompilation(CompilationUnit unit) {
     return new ASTCompilation(unit, new Tree(""));
   }
 
+  /**
+   * Create ASTCompilation from CompilationUnit of Eclipse AST and root Tree.
+   * 
+   * @param unit
+   *          CompilationUnit of Eclipse AST
+   * @param root
+   *          root Tree
+   */
   public ASTCompilation(CompilationUnit unit, Tree root) {
     this.root = root;
     if (unit.getPackage() != null) {
@@ -54,10 +99,22 @@ public class ASTCompilation implements Treeable {
     addTypes(unit);
   }
 
+  /**
+   * ?????
+   * 
+   * @param baseDir
+   *          ????
+   * @return ?????
+   */
   public List<String> getChangedFileList(File baseDir) {
     return root.getObjectsPath("");
   }
 
+  /**
+   * returns root of classes.
+   * 
+   * @return root of classes
+   */
   private Tree getClassRoot() {
     if (classRoot == null) {
       classRoot = new Tree(CLASS_ROOT_NAME);
@@ -84,6 +141,12 @@ public class ASTCompilation implements Treeable {
   // return pack.getLeaf();
   // }
 
+  /**
+   * add classes and interfaces to compilation unit.
+   * 
+   * @param unit
+   *          compilation unit of Eclipse AST
+   */
   public void addTypes(CompilationUnit unit) {
     for (Object o : unit.types()) {
       TypeDeclaration typeDec = (TypeDeclaration) o;
